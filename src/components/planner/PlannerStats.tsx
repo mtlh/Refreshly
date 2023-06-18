f0d725d177c549241ee966530d0f900c0e629a22
Matthew Harvey <matthewtlharvey@gmail.com>
2023-06-18 16:11:15 +0100

    planner - stats pie chart test

--- src/components/planner/PlannerStats.tsx
+++ src/components/planner/PlannerStats.tsx
@@ -1,9 +1,67 @@
+import { batch, createEffect, createSignal, onMount } from 'solid-js'
+import { Chart, Title, Tooltip, Legend, Colors } from 'chart.js'
+import { Pie } from 'solid-chartjs'
+import { getEntities } from '~/functions/planner/getEntities'
+import { createStore } from 'solid-js/store'
+import { Entity } from '~/types_const/planner'
+import { Id } from '@thisbeyond/solid-dnd'
+
 export const PlannerStats = () => {
+
+    let nextOrder = 1;
+    let nextID = 1;
+    const [entities, setEntities] = createStore<Record<Id, Entity>>({});
+    const [names, setNames] = createSignal([])
+    
+    const [chartData, setChartData] = createSignal({
+        labels: [],
+        datasets: [{}],
+    });
+
+    const [chartOptions] = createSignal({
+        responsive: true,
+        maintainAspectRatio: false
+    });
+
+    onMount(() => {
+        Chart.register(Title, Tooltip, Legend, Colors)
+        batch(async () => {
+            let ent = await getEntities(nextID, nextOrder, entities, setEntities); nextID = ent.nextID; nextOrder = ent.nextOrder;
+            let tempnames = [];
+            for (let task in entities) {
+                tempnames.push(entities[task].name)
+            }; 
+            // @ts-ignore
+            setNames(tempnames);
+        });
+    })
+
+    createEffect(() => {
+        setChartData({
+            labels: names(),
+            datasets: [
+                {
+                    label: 'Sales',
+                    data: [50, 60, 70, 80, 90],
+                },
+            ],
+        });
+    }, [names])
+
     return (
         <>
-        <div class={""}>
-            Stats
-        </div>
+         {chartData().labels.toString() != "" ?
+            <>
+                <div class='grid grid-cols-2 h-80'>
+                    <div>
+                        <Pie data={chartData()} options={chartOptions()} width={"100%"} height={"100%"} />
+                    </div>
+                    <div></div>
+                </div>
+            </> 
+         :
+         <div></div>
+         }
         </>
-    );
-};
+    )
+}
