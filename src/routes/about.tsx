89e4a944aac18d86b5f5e0ebb7ecfd27096cfe55
Matthew Harvey <matthewtlharvey@gmail.com>
2023-06-23 09:53:56 +0100

    link preview on /about

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,31 +1,74 @@
-import { A } from "solid-start";
-import Counter from "~/components/Counter";
+import { createSignal, onCleanup } from 'solid-js';
+import { getLinkPreview } from 'link-preview-js';
+import server$ from "solid-start/server";
 
-export default function About() {
+function LinkPreview() {
+  const [link, setLink] = createSignal('');
+  const [previewData, setPreviewData] = createSignal(null);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // Generate the link preview
+    const data = await fetchLinkPreview(link());
+    console.log(data);
+    // @ts-ignore Update the previewData state
+    setPreviewData(data);
+  };
+
+  const fetchLinkPreview = server$(async (link) => {
+
+    let datareturn = {
+      url: "",
+      mediaType: "",
+      contentType: "",
+      favicons: new Array(),
+      title: "",
+      siteName: "",
+      description: "",
+      images: new Array(),
+    }
+
+    try {
+      await getLinkPreview(link, {
+        imagesPropertyType: "og", // fetches only open-graph images
+        headers: {
+          "user-agent": "googlebot", // fetches with googlebot crawler user agent
+          "Accept-Language": "fr-CA", // fetches site for French language
+          // ...other optional HTTP request headers
+        },
+        timeout: 1000
+      }).then(data => {datareturn.url = data.url; datareturn.title = data.title; datareturn.siteName = data.siteName; datareturn.images = data.images; datareturn.description = data.description;});
+
+      return datareturn;
+
+    } catch (error) {
+      console.error('Error fetching link preview:', error);
+      return datareturn;
+    }
+  });
+
   return (
-    <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
-        About Page
-      </h1>
-      <Counter />
-      <p class="mt-8">
-        Visit{" "}
-        <a
-          href="https://solidjs.com"
-          target="_blank"
-          class="text-sky-600 hover:underline"
-        >
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
-      </p>
-      <p class="my-4">
-        <A href="/" class="text-sky-600 hover:underline">
-          Home
-        </A>
-        {" - "}
-        <span>About Page</span>
-      </p>
-    </main>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={link()}
+          onInput={(e) => setLink(e.target.value)}
+          placeholder="Enter a link"
+        />
+        <button type="submit">Preview</button>
+      </form>
+
+      {previewData() && (
+        <div>
+          <h3>{previewData().title}</h3>
+          <p>{previewData().description}</p>
+          <img src={previewData().images[0]} alt="Link thumbnail" />
+        </div>
+      )}
+    </div>
   );
 }
+
+export default LinkPreview;
