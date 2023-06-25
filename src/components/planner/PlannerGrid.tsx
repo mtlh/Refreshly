4149eaa1cd910cfb0870b2c6165c6aa1a24360b2
Matthew Harvey <matthewtlharvey@gmail.com>
2023-06-25 16:06:13 +0100

    basic grid view

---
+++ src/components/planner/PlannerGrid.tsx
@@ -0,0 +1,130 @@
+import { For, batch, createEffect, createSignal, onMount } from "solid-js";
+import { createStore } from "solid-js/store";
+import { Id } from "@thisbeyond/solid-dnd";
+import { Entity, Group, Item } from "~/types_const/planner";
+import { getEntities } from "~/functions/planner/getEntities";
+import { getProgressChoice } from "~/functions/planner/progressChoice";
+import { getPriorityChoice } from "~/functions/planner/priorityChoice";
+import moment from "moment";
+
+export const PlannerGrid = () => {
+    let nextOrder = 1;
+    let nextID = 1;
+
+    const [entities, setEntities] = createStore<Record<Id, Entity>>({});
+    const [progressChoice, setProgressChoice] = createSignal(new Array());
+    const [priorityChoice, setPriorityChoice] = createSignal(new Array());
+
+    const [allTask, setAllTask] = createSignal(new Array());
+    const [allGroup, setAllGroup] = createSignal(new Array());
+
+    const setup = () => {
+    batch(async () => {
+        let ent = await getEntities(nextID, nextOrder, entities, setEntities); nextID = ent.nextID; nextOrder = ent.nextOrder;
+        // SetBoardCol(await getBoardCol());
+        let progressChoice: string[] = await getProgressChoice();
+        setProgressChoice(progressChoice);
+        let priorityChoice: string[] = await getPriorityChoice();
+        setPriorityChoice(priorityChoice);
+    });
+    };
+    onMount(setup);
+    
+    createEffect(() => {
+        let allgroups = []; let alltasks = [];
+        for (var x in entities) { if (entities[x].type == "group") {allgroups.push(entities[x])} else {alltasks.push(entities[x])} nextID+=1; nextOrder+=1;};
+        setAllTask(alltasks); setAllGroup(allgroups);
+    }, [entities])
+
+    return (
+        <>
+            
+            <div class="relative overflow-x-auto m-2">
+                <table class="w-full text-sm text-left text-gray-500">
+                    <thead class="text-sm text-gray-700 uppercase bg-gray-200">
+                        <tr>
+                            <th scope="col" class="px-4 py-2">
+                                Task Name
+                            </th>
+                            <th scope="col" class="px-4 py-2">
+                                Group
+                            </th>
+                            <th scope="col" class="px-4 py-2">
+                                Priority
+                            </th>
+                            <th scope="col" class="px-4 py-2">
+                                Progress
+                            </th>
+                            <th scope="col" class="px-4 py-2">
+                                Startdate
+                            </th>
+                            <th scope="col" class="px-4 py-2">
+                                Duedate
+                            </th>
+                            <th scope="col" class="px-4 py-2">
+                                Description
+                            </th>
+                            <th scope="col" class="px-4 py-2">
+                                Checklist
+                            </th>
+                            <th scope="col" class="px-4 py-2">
+                                Last Updated
+                            </th>
+                            <th scope="col" class="px-4 py-2">
+                                Files
+                            </th>
+                            <th scope="col" class="px-4 py-2">
+                                Links
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <For each={allTask()}>{ (task: Item) => 
+                            <>
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {task.name}
+                                </th>
+                                <td class="px-4 py-2">
+                                    {task.group}
+                                </td>
+                                <td class="px-4 py-2">
+                                    {task.priority}
+                                </td>
+                                <td class="px-4 py-2">
+                                    {task.progress}
+                                </td>
+                                <td class="px-4 py-2">
+                                    {moment(task.startdate).format("DD/MM/YYYY")}
+                                </td>
+                                <td class="px-4 py-2">
+                                    {moment(task.duedate).format("DD/MM/YYYY")}
+                                </td>
+                                <td class="px-4 py-2">
+                                    {task.description}
+                                </td>
+                                <td class="px-4 py-2">
+                                    {/* {task.checklist.toString()} */}
+                                    Checklist
+                                </td>
+                                <td class="px-4 py-2">
+                                    {moment(task.lastupdate).format("hh:mm:ss DD/MM/YYYY")}
+                                </td>
+                                <td class="px-4 py-2">
+                                    {/* {task.externalfiles} */}
+                                    externalfiles
+                                </td>
+                                <td class="px-4 py-2">
+                                    {/* {task.externallinks.toString()} */}
+                                    externallinks
+                                </td>
+                            </tr>
+                            </>
+                        }</For>
+                    </tbody>
+                </table>
+            </div>
+
+        </>
+    );
+};
--- src/routes/planner.tsx
+++ src/routes/planner.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from "@solidjs/router";
 import { createSignal } from "solid-js";
 import { useSearchParams } from "solid-start";
 import { PlannerBoard } from "~/components/planner/PlannerBoard";
+import { PlannerGrid } from "~/components/planner/PlannerGrid";
 import { PlannerOptions } from "~/components/planner/PlannerOptions";
 import { PlannerStats } from "~/components/planner/PlannerStats";
 import { PlannerTimeline } from "~/components/planner/PlannerTimeline";
@@ -13,15 +14,15 @@ export default function Planner() {
   if (searchParams.options) {
     SetFormat("options")
   } 
-  // else if ( searchParams.list) {
-  //   SetFormat("list")
-  // } 
+  else if ( searchParams.grid) {
+    SetFormat("grid")
+  } 
   else if ( searchParams.board) {
     SetFormat("board")
   } 
-  else if ( searchParams.timeline) {
-    SetFormat("timeline")
-  } 
+  // else if ( searchParams.timeline) {
+  //   SetFormat("timeline")
+  // } 
   else if ( searchParams.stats) {
     SetFormat("stats")
   }
@@ -30,20 +31,20 @@ export default function Planner() {
       <p class="m-2 p-2 text-4xl font-bold text-left w-full">Your Planner</p>
       <div class="flex flex-row text-left m-2 p-2 gap-4 md:gap-10 underline underline-color text-lg md:text-xl font-normal">
         <button onclick={()=> {SetFormat("board"); nav("/planner?board=true")}}>Board</button>
-        {/* <button onclick={()=> {SetFormat("list"); nav("/planner?list=true")}}>List</button> */}
-        <button onclick={()=> {SetFormat("timeline"); nav("/planner?timeline=true")}}>Timeline</button>
+        <button onclick={()=> {SetFormat("grid"); nav("/planner?grid=true")}}>Grid</button>
+        {/* <button onclick={()=> {SetFormat("timeline"); nav("/planner?timeline=true")}}>Timeline</button> */}
         <button onclick={()=> {SetFormat("stats"); nav("/planner?stats=true")}}>Stats</button>
         <button onclick={()=> {SetFormat("options"); nav("/planner?options=true")}}>Options</button>
       </div>
       { format() == "board" &&
           <PlannerBoard type="board" />
       }
-      {/* { format() == "list" &&
-          <PlannerBoard type="list" />
-      } */}
-      { format() == "timeline" &&
-          <PlannerTimeline />
+      { format() == "grid" &&
+          <PlannerGrid />
       }
+      {/* { format() == "timeline" &&
+          <PlannerTimeline />
+      } */}
       { format() == "stats" &&
           <PlannerStats />
       }
